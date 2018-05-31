b77731223a30af17739a2954483ba02eba592049
Rodrigo Cunha <rodrigo.cunha@wiline.com>
2018-05-31 06:32:43 -0700

    Added the props to the bookshelves and started working on the Book Component Filtering

--- src/App.js
+++ src/App.js
@@ -5,16 +5,10 @@ import './App.css'
 import BookShelf from './BookShelf'
 import OpenSearch from './OpenSearch'
 import SearchBar from './SearchBar'
+import Book from './Book'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: []
   }
 
@@ -25,13 +19,38 @@ class BooksApp extends React.Component {
     })
   }
 
+  getCurrentlyReading = (shelf) => {
+      let arr = []
+      if (this.state.books.shelf === shelf) {
+        arr.push(this.state.books)
+      }
+      return arr
+  }
+
+
   render() {
+           console.log(this.getCurrentlyReading('read'))
     return (
       <div className="app">
         <Route path="/search" component={SearchBar} />
           <Route exact path="/" render={ () => (
             <div>
-              <BookShelf />
+              <BookShelf title="Currently Reading" />
+                <ol className="books-grid">
+                  <Book />
+                  <Book />
+                </ol>
+
+              <BookShelf title="Want to Read" />
+                <ol className="books-grid">
+                  <Book />
+                  <Book />
+                </ol>
+              <BookShelf title="Read" />
+                <ol className="books-grid">
+                  <Book />
+                  <Book />
+                </ol>
               <OpenSearch />
             </div>
           )} />
